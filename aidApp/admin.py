8ec20e348a1e6d24c366e66a4d0ea8cb3b6c365f
michaleb <michale.bailey@gmail.com>
2021-09-26 16:33:56 -0400

    Added Medical_History class

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -1,5 +1,5 @@
 from email.headerregistry import Group
-from aidApp.models import Health_Practitioner, Appointment, Feedback, Contact, FAQ, Patient, Pharmacy, Clinic  
+from aidApp.models import Health_Practitioner, Appointment, Feedback, Contact, FAQ, Patient, Pharmacy, Clinic, Medical_History  
 from django.contrib import admin
 from .models import FAQ
 from django.contrib.auth.models import User, Group
@@ -13,6 +13,7 @@ admin.site.register(FAQ)
 admin.site.register(Contact)
 admin.site.register(Feedback)
 admin.site.register(Appointment)
+admin.site.register(Medical_History)
 
 class Patients_Admin(admin.ModelAdmin):
     list_display = ('__str__', 'D_O_B', 'telephone')
