fb4be2a287b1ed1edebacdd7544cb740dfd23921
michaleb <michale.bailey@gmail.com>
2021-09-29 13:43:34 -0400

    Added paths for in template navigation

--- patient/urls.py
+++ patient/urls.py
@@ -5,6 +5,9 @@ from .views import (
     support_success_view,
     patient_dash_view,
     patient_doctor_view,
+    patient_profile_view,
+    patient_clinic_view,
+    clinic_info_view,
     DocProfile,
     CreateAppointment,
 )
@@ -14,6 +17,9 @@ urlpatterns = [
     path('support-success/', support_success_view, name = "support-success"),
     path('patient-dash/', patient_dash_view, name = "patient-dash"),
     path('patient-doctor/', patient_doctor_view, name = "patient-doctor"),
+    path('patient-profile/', patient_profile_view,name = "patient-profile"),
+    path('patient-clinic/', patient_clinic_view, name = "patient-clinic"),
+    path('clinic-info/<str:location>/<int:pk>/', clinic_info_view, name = "clinic-info"),
     path('patient-appt/<int:id>', CreateAppointment, name='patient-appt'), #
     path('patient-doctor-profile/<int:id>', DocProfile, name='patient-doctor-profile'),
         
