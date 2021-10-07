cc6a7ddce61ec6089876f1734241656b1d89c99c
michaleb <michale.bailey@gmail.com>
2021-10-07 02:31:09 -0400

    Registered emergency contact info model in admin.py

--- aidApp/admin.py
+++ aidApp/admin.py
@@ -1,5 +1,5 @@
 from email.headerregistry import Group
-from aidApp.models import Health_Practitioner, Appointment, Feedback, Contact, FAQ, Patient, Pharmacy, Clinic, Medical_History, Patient_Contact_Info  
+from aidApp.models import Health_Practitioner, Appointment, Feedback, Contact, FAQ, Patient, Pharmacy, Clinic, Medical_History, Patient_Contact_Info, Emergency_Contact_Info  
 from django.contrib import admin
 from .models import FAQ
 from django.contrib.auth.models import User, Group
@@ -15,6 +15,7 @@ admin.site.register(Feedback)
 admin.site.register(Appointment)
 admin.site.register(Medical_History)
 admin.site.register(Patient_Contact_Info)
+admin.site.register(Emergency_Contact_Info)
 
 class Patients_Admin(admin.ModelAdmin):
     list_display = ('__str__', 'D_O_B', 'telephone')
